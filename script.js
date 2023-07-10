464972820bc1a0f6170b449ef8e9ec2fe70be799
Riley <riley.childs@yahoo.com>
2023-07-10 19:17:14 -0400

    Finished making mobile nav responsive

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-const topNav = document.getElementsByClassName("nav--top")[0];
-const topLinks = document.getElementsByClassName("nav__links--top")[0];
-const topLink = document.getElementsByClassName("nav__link--top");
-const topAnchor = document.getElementsByClassName("nav__anchor--top");
+let isOpen;
 
-const navBtn = document.getElementsByClassName("toggle-nav")[0];
+const topNav = {
+  container: document.getElementsByClassName("nav--top")[0],
+  links: document.getElementsByClassName("nav__links--top")[0],
+  link: document.getElementsByClassName("nav__link--top"),
+  anchor: document.getElementsByClassName("nav__anchor--top")
+}
 
 const toggleIcon = {
+  btn: document.getElementsByClassName("toggle-nav")[0],
   open: document.getElementsByClassName("toggle-nav__icon--open")[0],
   close: document.getElementsByClassName("toggle-nav__icon--close")[0],
 };
 
-navBtn.addEventListener("click", () => {
-  if (!topNav.classList.contains("nav--mobile")) {
-    topNav.classList.add("nav--mobile");
-    topLinks.classList.add("nav__links--mobile");
-    for(let x = 0; x <topLink.length; x++){
-      topLink[x].classList.add("nav__link--mobile");
-    }
-    for(let x = 0; x <topAnchor.length; x++){
-        topAnchor[x].classList.add("nav__anchor--mobile");
-      }
+function toggleStyle(action) {
+  topNav.container.classList[action]("nav--mobile");
+  topNav.links.classList[action]("nav__links--mobile");
+  for (let x = 0; x < topNav.link.length; x++) {
+    topNav.link[x].classList[action]("nav__link--mobile");
+  }
+  for (let x = 0; x < topNav.anchor.length; x++) {
+    topNav.anchor[x].classList[action]("nav__anchor--mobile");
+  }
+}
+
+toggleIcon.btn.addEventListener("click", () => {
+  if (!topNav.container.classList.contains("nav--mobile")) {
+    isOpen = true;
+    toggleStyle("add");
     toggleIcon.open.style.display = "none";
     toggleIcon.close.style.display = "block";
   } else {
-    topNav.classList.remove("nav--mobile");
-    topLinks.classList.remove("nav__links--mobile");
-    for(let x = 0; x <topLink.length; x++){
-        topLink[x].classList.remove("nav__link--mobile");
-      }
-      for(let x = 0; x <topAnchor.length; x++){
-        topAnchor[x].classList.remove("nav__anchor--mobile");
-      }
+    isOpen = false;
+    toggleStyle("remove");
     toggleIcon.open.style.display = "block";
     toggleIcon.close.style.display = "none";
+  }
+});
+
+window.addEventListener("resize", (e) => {
+  const width = e.target.innerWidth;
+  if (width > 716 && isOpen) {
+    toggleStyle("remove");
+  } else if(width < 716 && isOpen){
+    toggleStyle("add");
   }
 });
